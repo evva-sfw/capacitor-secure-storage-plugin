fa4e463b8f4506261c9a982f545c075ea38392f1
PALIC Aleksandar / EVVA Wien <a.palic@evva.com>
2024-08-28 10:24:13 +0200

    chore: add editorconfig and eslint config

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,8 +1,12 @@
 export interface SecureStoragePluginPlugin {
-  get(options: { key: string, accessibility?: string }): Promise<{ value: string }>;
-  set(options: { key: string; value: string, accessibility?: string }): Promise<{ value: boolean }>;
-  remove(options: { key: string, accessibility?: string }): Promise<{ value: boolean }>;
-  clear(): Promise<{ value: boolean }>;
-  keys(): Promise<{ value: string[] }>;
-  getPlatform(): Promise<{ value: string }>;
+  get: (options: { key: string; accessibility?: string }) => Promise<{ value: string }>;
+  set: (options: {
+    key: string;
+    value: string;
+    accessibility?: string;
+  }) => Promise<{ value: boolean }>;
+  remove: (options: { key: string; accessibility?: string }) => Promise<{ value: boolean }>;
+  clear: () => Promise<{ value: boolean }>;
+  keys: () => Promise<{ value: string[] }>;
+  getPlatform: () => Promise<{ value: string }>;
 }
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,9 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { SecureStoragePluginPlugin } from './definitions';
 
-const SecureStoragePlugin = registerPlugin<SecureStoragePluginPlugin>(
-  'SecureStoragePlugin',
-  {
-    web: () => import('./web').then(m => new m.SecureStoragePluginWeb()),
-  },
-);
+const SecureStoragePlugin = registerPlugin<SecureStoragePluginPlugin>('SecureStoragePlugin', {
+  web: async () => await import('./web').then((m) => new m.SecureStoragePluginWeb()),
+});
 
 export * from './definitions';
 export { SecureStoragePlugin };
